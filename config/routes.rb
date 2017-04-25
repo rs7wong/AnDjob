29201021bfb1570cb4be8900bbfafebc0b19ab6b
rswong <ziud7rs@gmail.com>
2017-04-25 20:17:23 +0800

    main image setting

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  root 'jobs#index'
+  root 'welcome#index'
 
 
 end
